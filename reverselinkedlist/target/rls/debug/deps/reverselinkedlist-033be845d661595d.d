53f181e2c7f8ae13b9a8d833562c831889aed57d
alan <falloonalan@gmail.com>
2017-12-19 21:48:51 +1300

    inital commit

---
+++ reverselinkedlist/target/rls/debug/deps/reverselinkedlist-033be845d661595d.d
@@ -0,0 +1,5 @@
+/home/alan/rust/reverselinkedlist/target/rls/debug/deps/reverselinkedlist-033be845d661595d.rmeta: rust/reverselinkedlist/src/main.rs
+
+/home/alan/rust/reverselinkedlist/target/rls/debug/deps/reverselinkedlist-033be845d661595d.d: rust/reverselinkedlist/src/main.rs
+
+rust/reverselinkedlist/src/main.rs:
---
+++ reverselinkedlist/target/rls/debug/libreverselinkedlist.d
@@ -0,0 +1,1 @@
+/home/alan/rust/reverselinkedlist/target/rls/debug/libreverselinkedlist.rmeta: /home/alan/rust/reverselinkedlist/src/main.rs
